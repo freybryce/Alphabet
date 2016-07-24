f5eba58e926fcc611103113b54927603529c03f1
Bryce <demouser@D-173-250-161-176.dhcp4.washington.edu>
2016-07-24 14:38:23 -0700

    Added a templates folder with a barebones template for the front page as well as some handler adjustments

--- alphabet/main.py
+++ alphabet/main.py
@@ -8,11 +8,7 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 #
 
 # import necessary python libraries for running app engine, editing html, targeting file in the OS, logging issues, etc.
@@ -29,39 +25,38 @@ import jinja2
 # from google.appengine.api import users
 
 # Make sure to use relative path for file path calls, that is how this version of the jinja Environment is set up. Also check copied code for 'jinja_env' vs 'jinja_environment'
-# jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
+    # This handler manages the front page and should be connected to 'frontpage.html'
     def get(self):
-        self.response.write('Hello world!')
+        main_template = jinja_env.get_template('templates/frontpage.html')
+        self.response.out.write(main_template.render())
 
 class RedditResultsHandler(webapp2.RequestHandler):
-    # This handler is designed to process requests reddit search results
-    # Not sure if we are using the get method, the post method or both yet
+    # This handler is designed to process requests reddit search results. Not sure if we are using the get method, the post method or both yet
     def get(self):
         self.response.write('Hello world! This is the Reddit Handler!')
 
 class FacebookResultsHandler(webapp2.RequestHandler):
-    # This handler is designed to process requests Facebook search results
-    # Not sure if we are using the get method, the post method or both yet
+    # This handler is designed to process requests Facebook search results. Not sure if we are using the get method, the post method or both yet
     def get(self):
         self.response.write('Hello world! This is the Facebook Handler!')
 
 class TwitterResultsHandler(webapp2.RequestHandler):
-    # This handler is designed to process requests twitter search results
-    # Not sure if we are using the get method, the post method or both yet
+    # This handler is designed to process requests twitter search results. Not sure if we are using the get method, the post method or both yet
     def get(self):
         self.response.write('Hello world! This is the Twitter Handler!')
 
 class GiphyResultsHandler(webapp2.RequestHandler):
-    # This handler is designed to process requests Giphy search results
-    # Not sure if we are using the get method, the post method or both yet
+    # This handler is designed to process requests Giphy search results. Not sure if we are using the get method, the post method or both yet
     def get(self):
         self.response.write('Hello world! This is the Giphy Handler!')
 
 class DefaultHandler(webapp2.RequestHandler):
+    # This handler should be designed to give the user a page that incourages them to go to the front page ('/') for all cases where the page route is not one of the predefined routes. When we get to it, we should create an HTML template for it instead of just writing onto the page as it is now.
     def get(self):
-        self.response.write('<h1>404 NOT FOUND</h2><p>Try "/r"</p>')
+        self.response.write('<h1>404 NOT FOUND</h2><br><p>Try "/"</p>')
 
 routes = [
     ('/', MainHandler),
@@ -69,8 +64,13 @@ routes = [
     ('/facebook', FacebookResultsHandler),
     ('/twitter', TwitterResultsHandler),
     ('/giphy', GiphyResultsHandler),
+    # The following are reserved URL paths and their potential handlers. Don't know if we will need them yet, but just incase...
     #
-    # DO NOT put anything below default handler, otherwise site requests go heywire
+    # ('/login', LoginHandler),
+    # ('/logout, LogoutHandler'),
+    # ('/signup', SignupHandler),
+    #
+    # DO NOT put anything below default handler, otherwise site requests go haywire
     #
     ('/.*', DefaultHandler),
 ]
