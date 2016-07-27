682227b4534aef82a0ca146fd1655dde6b621cc2
ayee <amy99658356@gmail.com>
2016-07-27 12:15:16 -0700

    added css and js files for front page

---
+++ alphabet/static/frontpage.js
@@ -0,0 +1,14 @@
+$('input')
+
+/*
+$('img').mouseover(changeSize)
+
+function changeColor() {
+  $('body').css('background', 'white');
+}
+
+function changeSize() {
+  $(this).animate({width: '100%'}, 1500)
+  window.setTimeout(changeColor, 1500);
+}
+*/
